9b63a754ca13d37ecafd53cbb2bbe68540b01b7c
HyunsooZo <bzhs1992@icloud.com>
2023-11-10 00:15:32 +0900

    Feat: 고객 정보가 없을 경우 반환할 예외 추가

--- src/main/java/com/mybudget/exception/ErrorCode.java
+++ src/main/java/com/mybudget/exception/ErrorCode.java
@@ -4,14 +4,17 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
     //undefined
-    UNDEFINED_EXCEPTION(BAD_REQUEST, "알 수 없는 오류입니다.");
+    UNDEFINED_EXCEPTION(BAD_REQUEST, "알 수 없는 오류입니다."),
+    //user
+    USER_INFO_NOT_FOUND(NOT_FOUND, "사용자 정보를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
