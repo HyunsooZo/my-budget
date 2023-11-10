67ab1a4fb38165423a4570e55a8d8e881f9c0d1f
HyunsooZo <bzhs1992@icloud.com>
2023-11-10 10:32:21 +0900

    Feat: 이메일 비동기 처리로 api 응답 속도 개선

--- src/main/java/com/mybudget/component/EmailSender.java
+++ src/main/java/com/mybudget/component/EmailSender.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailSender {
     private final JavaMailSender javaMailSender;
+
+    @Async
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
