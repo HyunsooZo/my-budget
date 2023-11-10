79426c7883eb7f86d95c91cdb69145e90aee82e8
HyunsooZo <bzhs1992@icloud.com>
2023-11-11 00:49:54 +0900

    Feat: 사용자 로그인 관련 예외 추가

--- src/main/java/com/mybudget/exception/ErrorCode.java
+++ src/main/java/com/mybudget/exception/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
     //user
     USER_INFO_NOT_FOUND(NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
     EXISTING_USER(BAD_REQUEST, "이미 가입한 사용자 입니다."),
-    INVALID_OTP(BAD_REQUEST, "유효하지 않은 인증번호 입니다.");
+    INVALID_OTP(BAD_REQUEST, "유효하지 않은 인증번호 입니다."),
+    INVALID_PASSWORD(BAD_REQUEST,"비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
