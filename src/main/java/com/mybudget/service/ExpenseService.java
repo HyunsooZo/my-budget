ab6bd593496e42cabed5e3be05adc891a4af1e66
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-11-12 10:00:07 +0900

    Merge: 지출을 등록하는 기능 추가 (#12)
    * Feat: 지출 등록 기능 추가
    
    * Test: 지출 등록 관련 테스트코드 추가

--- src/main/java/com/mybudget/service/ExpenseService.java
+++ src/main/java/com/mybudget/service/ExpenseService.java
@@ -1,11 +1,51 @@
 package com.mybudget.service;
 
+import com.mybudget.domain.Expense;
+import com.mybudget.domain.User;
+import com.mybudget.dto.ExpenseCreationRequestDto;
+import com.mybudget.exception.CustomException;
 import com.mybudget.repository.ExpenseRepository;
+import com.mybudget.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+import static com.mybudget.exception.ErrorCode.USER_INFO_NOT_FOUND;
 
 @RequiredArgsConstructor
 @Service
 public class ExpenseService {
     private final ExpenseRepository expenseRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 사용자의 지출 내역 생성
+     *
+     * @param userId 사용자 식별자
+     * @param expenseCreationRequestDto 지출 생성 요청 DTO
+     *
+     * @throws CustomException 사용자 정보를 찾을 수 없는 경우 예외를 발생시킵니다.
+     */
+    @Transactional
+    public void createExpense(Long userId, ExpenseCreationRequestDto expenseCreationRequestDto) {
+
+        User user = getUser(userId);
+
+        expenseRepository.save(Expense.from(user, expenseCreationRequestDto));
+    }
+
+    /**
+     * 주어진 사용자 식별자에 해당하는 사용자 조회
+     *
+     * @param userId 사용자 식별자
+     * @return 주어진 식별자에 해당하는 사용자
+     *
+     * @throws CustomException 사용자 정보를 찾을 수 없는 경우 예외를 발생시킵니다.
+     */
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_INFO_NOT_FOUND));
+    }
+
 }
---
+++ src/test/java/com/mybudget/service/ExpenseCreationTest.java
@@ -0,0 +1,69 @@
+package com.mybudget.service;
+
+import com.mybudget.config.UserRole;
+import com.mybudget.domain.User;
+import com.mybudget.dto.ExpenseCreationRequestDto;
+import com.mybudget.enums.UserStatus;
+import com.mybudget.repository.ExpenseRepository;
+import com.mybudget.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import static com.mybudget.enums.Categories.EDUCATION;
+import static org.mockito.Mockito.*;
+
+@DisplayName("지출 생성 테스트")
+class ExpenseCreationTest {
+
+    @Mock
+    private ExpenseRepository expenseRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private ExpenseService expenseService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        expenseService = new ExpenseService(expenseRepository, userRepository);
+    }
+
+    static ExpenseCreationRequestDto expenseCreationRequestDto =
+            ExpenseCreationRequestDto.builder()
+                    .excluding(false)
+                    .expenseDate(new Date(2023, 1, 1))
+                    .description("메모")
+                    .amount(BigDecimal.valueOf(100000))
+                    .category(EDUCATION)
+                    .build();
+
+    static User user = User.builder()
+            .id(1L)
+            .email("email@test.com")
+            .phoneNumber("112333333")
+            .password("aaaaa")
+            .userStatus(UserStatus.ACTIVE)
+            .userRole(UserRole.ROLE_USER)
+            .build();
+
+    @Test
+    @DisplayName("성공")
+    public void createExpense_success() {
+        //given
+        when(userRepository.findById(1L))
+                .thenReturn(java.util.Optional.ofNullable(user));
+
+        //when
+        expenseService.createExpense(1L, expenseCreationRequestDto);
+
+        //then
+        verify(expenseRepository).save(any());
+    }
+}
