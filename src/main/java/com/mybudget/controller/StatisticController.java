ee66bff9b6c5647e457db2d34ed84e2bbaeb731f
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-11-16 04:48:13 +0900

    Merge: 통계-(1) 지난달 대비 카테고리별 소비비율 반환하는 기능 추가  (#17)
    * Feat: 지출 등록 시 해당 날짜의 예산 총액을 함께 저장하도록 수정
    
    이는 추후 통계에 사용하기위한 필드
    
    * Feat: 지출 엔티티 내 필드 추가로 인한 테스트코드 수정
    
    * Feat: 통계-(1) 지난달 대비 카테고리별 소비비율 반환하는 기능 추가
    
    1. 금일 날짜부터 30일 전, 그리고 그전 30일 의 지출목록을 가져와서
    2. 각 카테고리 별 금액을 Map 자료구조에 담고
    3. 해당 데이터를 DTO에 담아 반환
    
    * Feat: 통계에 사용될 지정된 예산대비 소비 비율 필드 추가
    
    * Test: 카테고리별 통계조회 테스트코드 추가

--- src/main/java/com/mybudget/controller/StatisticController.java
+++ src/main/java/com/mybudget/controller/StatisticController.java
@@ -1,13 +1,51 @@
 package com.mybudget.controller;
 
+import com.mybudget.config.JwtProvider;
+import com.mybudget.dto.CategoryExpenseRatioDto;
+import com.mybudget.dto.StatisticByCategoryResponseDto;
+import com.mybudget.service.StatisticService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.OK;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/statistics")
 @Api(tags = "Statistic API", description = "통계와 관련된 API")
 @RestController
 public class StatisticController {
+
+    private final StatisticService statisticService;
+    private final JwtProvider jwtProvider;
+
+    @GetMapping
+    @ApiOperation(value = "통계 조회", notes = "통계를 조회")
+    public ResponseEntity<StatisticByCategoryResponseDto> getStatistics(
+            @RequestHeader(AUTHORIZATION) String token) {
+
+        Long userId = jwtProvider.getIdFromToken(token);
+
+
+        List<CategoryExpenseRatioDto> categoryStatistics =
+                statisticService.getCategoryStatistics(
+                        userId, Date.valueOf(LocalDate.now())
+                );
+
+        StatisticByCategoryResponseDto statistics = StatisticByCategoryResponseDto.builder()
+                .ExpenseRatiosPerCategory(categoryStatistics)
+                .build();
+
+        return ResponseEntity.status(OK).body(statistics);
+    }
 }
