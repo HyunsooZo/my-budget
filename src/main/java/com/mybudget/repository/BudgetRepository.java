1a5cddd8f3aa705a9bb44ced5d337ede69123482
HyunsooZo <bzhs1992@icloud.com>
2023-11-17 02:40:31 +0900

    Feat: 일부 잘못된 import문 수정

--- src/main/java/com/mybudget/repository/BudgetRepository.java
+++ src/main/java/com/mybudget/repository/BudgetRepository.java
@@ -3,7 +3,7 @@ package com.mybudget.repository;
 import com.mybudget.domain.Budget;
 import com.mybudget.domain.User;
 import com.mybudget.enums.Categories;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
