6ce87a818569499bc2ea500112c9666e6100e8e1
HyunsooZo <bzhs1992@icloud.com>
2023-11-18 19:40:01 +0900

    Feat: 당일 지출 컨설팅 기능 추가 (매일아침 8시 알림)

--- src/main/java/com/mybudget/component/CronScheduler.java
+++ src/main/java/com/mybudget/component/CronScheduler.java
@@ -5,8 +5,7 @@ import com.mybudget.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
@@ -22,8 +21,14 @@ public class CronScheduler {
     }
 
     @Scheduled(cron = "0 0 20 * * *") // 매일 20시에 실행
-    @Transactional
+    @Transactional(readOnly = true)
     public void notifyTodayExpense() {
         expenseService.notifyTodayExpense();
+    }
+
+    @Scheduled(cron = "0 0 8 * * *") // 매일 08시에 실행
+    @Transactional(readOnly = true)
+    public void recommendDailyExpenses() {
+        expenseService.recommendExpenses();
     }
 }
