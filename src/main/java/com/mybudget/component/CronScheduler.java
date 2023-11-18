a283383a0d72a604f19b4460606bbbfefda6612d
HyunsooZo <bzhs1992@icloud.com>
2023-11-18 13:52:14 +0900

    Feat: 금일 지출내역 알림 기능 추가

--- src/main/java/com/mybudget/component/CronScheduler.java
+++ src/main/java/com/mybudget/component/CronScheduler.java
@@ -1,6 +1,7 @@
 package com.mybudget.component;
 
 import com.mybudget.service.BudgetRecommendationService;
+import com.mybudget.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,17 @@ import javax.transaction.Transactional;
 public class CronScheduler {
 
     private final BudgetRecommendationService budgetRecommendationService;
+    private final ExpenseService expenseService;
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 00시에 실행
     @Transactional
     public void calculateAndSaveCategoryRatio() {
         budgetRecommendationService.setRatiosPerBudgets();
+    }
+
+    @Scheduled(cron = "0 0 20 * * *") // 매일 20시에 실행
+    @Transactional
+    public void notifyTodayExpense() {
+        expenseService.notifyTodayExpense();
     }
 }
