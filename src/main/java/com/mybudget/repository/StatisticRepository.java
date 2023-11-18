ce8f0f1103f51aa512bd709d1fa54d497f3a4432
HyunsooZo <bzhs1992@icloud.com>
2023-11-18 20:10:44 +0900

    Remove: 사용하지 않는 클래스 삭제

--- src/main/java/com/mybudget/repository/StatisticRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mybudget.repository;
-
-import com.mybudget.domain.Statistic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatisticRepository extends JpaRepository<Statistic, Long> {
-}
